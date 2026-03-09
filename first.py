5a150553f59ec031c298e16f4ed33c6e7b674ea8
Siddharth <sgajbhiye96@gmail.com>
2026-03-09 08:18:49 +0530

    new code added

--- first.py
+++ first.py
@@ -7,3 +7,11 @@ def even_odd(num):
 
 print(even_odd(10))  # Output: Even
 print(even_odd(7))   # Output: Odd
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
